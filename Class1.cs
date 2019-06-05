1d3532b85b3deca081dc51d850333c32e1ed4f70
ksolo <krishna.solanki@mail.utoronto.ca>
2019-06-04 23:22:05 -0400

    Added new class for button events and read username and password

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using System;
+namespace Core
+{
+    public static class Splice
+    {
+        public static string Login(string a, string b)
+        {
+            if (string.IsNullOrWhiteSpace(a) && string.IsNullOrWhiteSpace(b))
+                return "Did not enter username or password!";
+            else
+                raw = raw.ToUpperInvariant();
