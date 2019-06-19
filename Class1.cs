1688862def12dda2cedad27b840ba41e4a34851d
ksolo <krishna.solanki@mail.utoronto.ca>
2019-06-18 23:48:58 -0400

    Updated login page

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,13 @@ namespace Core
     {
         public static string Login(string a, string b)
         {
-            if (string.IsNullOrWhiteSpace(a) && string.IsNullOrWhiteSpace(b))
+            if (string.IsNullOrWhiteSpace(a) || string.IsNullOrWhiteSpace(b))
+            {
                 return "Did not enter username or password!";
+            }
+
             else
-                raw = raw.ToUpperInvariant();
+            {
+                rawa = raw.ToUpperInvariant(a);
+                rawb = raw.ToUpperInvariant(b);
+            }
